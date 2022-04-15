1cc1d76ecab8b4fcdae83692e845aaf476467d3c
tjs6595 <t.schultejans@schenckprocess.com>
2022-04-15 12:21:32 -0500

    This is my Web Game Part 1 Submission with Bonus.

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(source, position, left, bottom){
+    let object = document.createElement('img')
+    object.src = source;
+    object.style.position = 'fixed';
+    object.style.left = left;
+    object.style.bottom = bottom;
+    document.body.append(object);
+    return object;
+}
+
+function newItem(source, position, left, bottom){
+    let object = newImage(source, position, left, bottom)
+
+    object.addEventListener('dblclick', function() {
+        object.remove()
+    })
+}
+
+for (let i=0; i<(window.innerWidth/100); i += 1){
+    for(j=5; j<(window.innerHeight/100); j += 1){
+        newImage('assets/sky.png', 'fixed', (i*100) + 'px', (j*100) + 'px');
+    }
+}
+
+for (let i=0; i<(window.innerWidth/100); i += 1){
+    for(j=0; j<((window.innerHeight/100)-3); j += 1){
+        newImage('assets/grass.png', 'fixed', (i*100) + 'px', (j*100) + 'px');
+    }
+}
+
+newImage('assets/green-character.gif', 'fixed', '100px', '100px');
+newImage('assets/pine-tree.png', 'fixed', '450px', '200px');
+newImage('assets/tree.png', 'fixed', '200px', '300px');
+newImage('assets/pillar.png', 'fixed', '350px', '100px');
+newImage('assets/crate.png', 'fixed', '150px', '200px');
+newImage('assets/well.png', 'fixed', '500px', '425px');
+
+newItem('assets/sword.png', 'fixed', '500px', '405px');
+newItem('assets/shield.png', 'fixed', '165px', '185px');
+newItem('assets/staff.png', 'fixed', '600px', '100px');
